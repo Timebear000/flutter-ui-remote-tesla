e5e35c8604e505c6658dd98b347d46cd80464fd7
timebear000 <timebear000@gmail.com>
2021-10-01 18:02:58 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tesla/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tesla Animated App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: LayoutBuilder(builder: (context, constrains) {
+          return Stack(
+            children: [
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(vertical: constrains.maxHeight * 0.1),
+                child: SvgPicture.asset(
+                  "assets/icons/Car.svg",
+                  width: double.infinity,
+                ),
+              ),
+              Positioned(
+                child: SvgPicture.asset("assets/icons/door_lock.svg"),
+              )
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
